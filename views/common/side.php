90beef5b799d87da96260e5fd15522c69cbf5933
otsu-kento <82933372+otsu-kento@users.noreply.github.com>
2021-07-01 19:41:35 +0900

    ログイン機能　実装

--- views/common/side.php
+++ views/common/side.php
@@ -7,7 +7,7 @@
             <li class="nav-item"><a href="notification.php" class="nav-link"><img src="<?= HOME_URL; ?>views/img/icon-notification.svg" alt="ベルアイコン"></a></li>
             <li class="nav-item"><a href="profile.php" class="nav-link"><img src="<?= HOME_URL; ?>views/img/icon-profile.svg" alt="プロフィールアイコン"></a></li>
             <li class="nav-item"><a href="post.php" class="nav-link"><img src="<?= HOME_URL; ?>views/img/icon-post-tweet-twitterblue.svg" alt="羽アイコン" class="post-tweet"></a></li>
-            <li class="nav-item my-icon"><img src="<?= HOME_URL; ?>views/img_uploaded/user/sample-person.jpg" class="js-popover" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="<a href='profile.php'>プロフィール</a><br><a href='sign-out.php'>ログアウト</a>" data-bs-html="true" alt="マイアイコン"></li>
+            <li class="nav-item my-icon"><img src="<?= htmlspecialchars($view_user['image_path']); ?>" class="js-popover" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="<a href='profile.php'>プロフィール</a><br><a href='sign-out.php'>ログアウト</a>" data-bs-html="true" alt="マイアイコン"></li>
         </ul>
     </div>
 </div>
