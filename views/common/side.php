7e52b5c57ebc91ef94e40bc40284670af039c6a6
otsu-kento <82933372+otsu-kento@users.noreply.github.com>
2021-08-26 10:03:50 +0900

    小文字から大文字

--- views/common/side.php
+++ views/common/side.php
@@ -7,7 +7,7 @@
             <li class="nav-item"><a href="Notification.php" class="nav-link"><img src="<?= HOME_URL; ?>views/img/icon-notification.svg" alt="ベルアイコン"></a></li>
             <li class="nav-item"><a href="Profile.php" class="nav-link"><img src="<?= HOME_URL; ?>views/img/icon-profile.svg" alt="プロフィールアイコン"></a></li>
             <li class="nav-item"><a href="Post.php" class="nav-link"><img src="<?= HOME_URL; ?>views/img/icon-post-tweet-twitterblue.svg" alt="羽アイコン" class="post-tweet"></a></li>
-            <li class="nav-item my-icon"><img src="<?= htmlspecialchars($view_user['image_path']); ?>" class="js-popover" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="<a href='profile.php'>プロフィール</a><br><a href='Sign-out.php'>ログアウト</a>" data-bs-html="true" alt="マイアイコン"></li>
+            <li class="nav-item my-icon"><img src="<?= htmlspecialchars($view_user['image_path']); ?>" class="js-popover" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="right" data-bs-content="<a href='Profile.php'>プロフィール</a><br><a href='Sign-out.php'>ログアウト</a>" data-bs-html="true" alt="マイアイコン"></li>
         </ul>
     </div>
 </div>
